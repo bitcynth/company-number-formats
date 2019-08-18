8c891c8a56c64d90c0df53cc02c010a34716aa72
Cynthia Revstrom <me@cynthia.re>
2019-08-18 17:46:18 +0200

    Fix csv parsing

--- test.py
+++ test.py
@@ -5,13 +5,14 @@ import csv
 
 country_formats = {}
 
-with open('formats.txt') as formats_file:
-    lines = formats_file.readlines()
-    for line in lines:
-        parts = line.split(',')
-        country = parts[0]
-        country_formats[country] = re.compile(parts[1])
+with open('formats.txt') as f:
+    reader = csv.reader(f, doublequote=True, quoting=csv.QUOTE_ALL, escapechar='\\')
+    for row in reader:
+        country = row[0]
+        country_formats[country] = re.compile(row[1])
+        print(row[1])
 
+# Sweden
 assert country_formats['se'].match('555555-1234'), 'SE: 555555-1234 is a valid Swedish organization number'
 assert country_formats['se'].match('5555551234'), 'SE: 5555551234 is a valid Swedish organization number (I think)'
 assert not country_formats['se'].match('555555-134'), 'SE: 555555-1234 is not a valid Swedish organization number'
