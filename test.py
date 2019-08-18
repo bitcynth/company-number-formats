77fc2525ec3269e7f96bd9b06fc12c884f2fc66d
Cynthia Revstrom <me@cynthia.re>
2019-08-18 17:52:16 +0200

    Remove bad print statement

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ with open('formats.txt') as f:
     for row in reader:
         country = row[0]
         country_formats[country] = re.compile(row[1])
-        print(row[1])
 
 # Sweden
 assert country_formats['se'].match('555555-1234'), 'SE: 555555-1234 is a valid Swedish organization number'
