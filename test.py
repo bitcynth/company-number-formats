146650e4d625e946932d2228ff5277a32673e2f7
Cynthia Revstrom <me@cynthia.re>
2019-08-18 18:01:04 +0200

    Add germany format (no source yet :( )

--- test.py
+++ test.py
@@ -15,3 +15,8 @@ with open('formats.txt') as f:
 assert country_formats['se'].match('555555-1234'), 'SE: 555555-1234 is a valid Swedish organization number'
 assert country_formats['se'].match('5555551234'), 'SE: 5555551234 is a valid Swedish organization number (I think)'
 assert not country_formats['se'].match('555555-134'), 'SE: 555555-1234 is not a valid Swedish organization number'
+
+# Germany
+assert country_formats['de'].match('HRB 151080'), 'DE: HRB 151080 is a valid German company number.'
+assert country_formats['de'].match('HRA 12345'), 'DE: HRA 12345 is a valid German company number.'
+assert not country_formats['de'].match('HRC 12345'), 'DE: HRC 12345 is not a valid German company number.'
